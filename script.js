4afd17a492599623ee8cdbc0c49b6f727c3e43d2
Solomon-Bright12 <solomonprincebright.com>
2024-06-05 16:56:58 +0000

    notes-app update

---
+++ script.js
@@ -0,0 +1,40 @@
+notedata = localStorage.getItem("notedata")
+  ? JSON.parse(localStorage.getItem("notedata"))
+  : [];
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let text = document.getElementById("notetextarea").value;
+  console.log(text);
+  note = {
+    textarea: text,
+    dateofnote: Date(),
+  };
+  notedata.push(note);
+  localStorage.setItem("notedata", JSON.stringify(notedata));
+
+  displayNote();
+});
+const displayNote = () => {
+  let div = document.getElementById("displayarea");
+  div.innerHTML = "";
+  notedata.forEach((note, index) => {
+    let number = index + 1;
+    const noteHTML = `
+    <div id="displayarea">
+      <p>${number}.${note.textarea}</p>
+      <button class="deletebtn" data-index=${index} onclick="deleteNotes()">Delete</button>
+      <p class="date">${note.dateofnote}</p>
+    </div>
+    `;
+
+    div.innerHTML += noteHTML; 
+  });
+};
+function deleteNotes(index) {
+  
+  notedata.splice(index,1);
+  localStorage.setItem("notedata", JSON.stringify(notedata));
+  displayNote();
+}
+
+displayNote();
